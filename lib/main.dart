6ad124196fd63f6e9c78a07dcf1e77f0efe83f65
Soo <soojlee0106@naver.com>
2024-11-01 21:51:47 +0900

    refactor: 공통 함수 임시 리팩토링

--- lib/main.dart
+++ lib/main.dart
@@ -28,65 +28,80 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
-  final items = List<String>.generate(20, (i) => 'Item ${i + 1}');
+  final items = List<String>.generate(5, (i) => 'Item ${i + 1}');
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Anxy Planner')),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            TableCalendar(
-              headerStyle: const HeaderStyle(
-                titleCentered: true,
-                formatButtonVisible: false,
-                leftChevronVisible: false,
-                rightChevronVisible: false,
-              ),
-              daysOfWeekHeight: 50,
-              focusedDay: _focusedDay,
-              availableGestures: AvailableGestures.none,
-              firstDay:
-                  DateTime.now().copyWith(month: DateTime.now().month - 1),
-              lastDay: DateTime.now().copyWith(month: DateTime.now().month + 1),
-              selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
-              onDaySelected: (selectedDay, focusedDay) {
-                if (!isSameDay(_selectedDay, selectedDay)) {
-                  setState(() {
-                    _selectedDay = selectedDay;
-                    _focusedDay = focusedDay;
-                  });
-                }
-              },
+      body: Column(
+        children: <Widget>[
+          TableCalendar(
+            headerStyle: const HeaderStyle(
+              titleCentered: true,
+              formatButtonVisible: false,
+              leftChevronVisible: false,
+              rightChevronVisible: false,
             ),
-            const SizedBox(height: 15),
-            Expanded(
-              child: ListView.builder(
-                itemCount: items.length,
-                itemBuilder: (context, index) {
-                  final item = items[index];
-                  return Dismissible(
-                    key: Key(item),
-                    onDismissed: (direction) {
-                      setState(() {
-                        items.removeAt(index);
-                      });
-                    },
-                    background: Container(color: Colors.red),
-                    child: CheckboxListTile(
-                      controlAffinity: ListTileControlAffinity.leading,
-                      title: Text(item),
-                      value: true,
-                      onChanged: (bool? value) {},
-                    ),
-                  );
-                },
+            daysOfWeekHeight: 50,
+            focusedDay: _focusedDay,
+            availableGestures: AvailableGestures.none,
+            firstDay: DateTime.now().copyWith(month: DateTime.now().month - 1),
+            lastDay: DateTime.now().copyWith(month: DateTime.now().month + 1),
+            selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+            onDaySelected: (selectedDay, focusedDay) {
+              if (!isSameDay(_selectedDay, selectedDay)) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              }
+            },
+          ),
+          const SizedBox(height: 15),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  _buildListView(title: '오늘 할 일'),
+                  _buildListView(title: '이번주 할 일'),
+                  _buildListView(title: '이번달 할 일'),
+                ],
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
+    );
+  }
+
+  ListView _buildListView({required String title}) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        if (index == 0) {
+          return ListTile(title: Text(title));
+        }
+
+        final item = items[index];
+        return Dismissible(
+          key: Key(item),
+          onDismissed: (direction) {
+            setState(() {
+              items.removeAt(index);
+            });
+          },
+          background: Container(color: Colors.red),
+          child: CheckboxListTile(
+            controlAffinity: ListTileControlAffinity.leading,
+            title: Text(item),
+            value: true,
+            onChanged: (bool? value) {},
+          ),
+        );
+      },
     );
   }
 }
