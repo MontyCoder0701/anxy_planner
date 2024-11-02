f78885a82d951df6761dc41563d0dafcc2ca00b5
Soo <soojlee0106@naver.com>
2024-11-02 10:39:12 +0900

    build: 앱 정보 수정

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'One Moon',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
       home: const HomeView(),
