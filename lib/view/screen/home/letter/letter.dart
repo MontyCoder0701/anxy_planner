ab3183883715f7d2e185fdffe264f92ef425683e
Soo <soojlee0106@naver.com>
2024-11-03 22:43:57 +0900

    feat: 편지함 화면 추가

--- lib/view/screen/home/letter/letter.dart
+++ lib/view/screen/home/letter/letter.dart
@@ -14,7 +14,48 @@ class _LetterScreenState extends State<LetterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          ListBody(
+            children: [
+              ListTile(
+                iconColor: CustomColor.primary,
+                textColor: CustomColor.primary,
+                leading: Icon(Icons.inbox),
+                title: Text(
+                  '나에게 받은 편지함',
+                  style: CustomTypography.bodyLarge.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text('안녕 나야'),
+                subtitle: Text('미래의 나야, 잘지냈니? ...'),
+                onTap: () {},
+              ),
+            ],
+          ),
+          Divider(),
+          ListBody(
+            children: [
+              ListTile(
+                iconColor: CustomColor.primary,
+                textColor: CustomColor.primary,
+                leading: Icon(Icons.move_to_inbox_rounded),
+                title: Text(
+                  '한달 후 보낼 편지함',
+                  style: CustomTypography.bodyLarge.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              ListTile(
+                title: Text('3통이 준비되었습니다.'),
+                subtitle: Text('기다리면 곧 만날거에요.'),
+              ),
+            ],
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         foregroundColor: Theme.of(context).colorScheme.surface,
