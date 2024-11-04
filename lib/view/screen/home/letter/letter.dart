4d17a0e0118dd180d09e6899702dc6013a3004b3
Soo <soojlee0106@naver.com>
2024-11-04 15:20:01 +0900

    fix: 타일 위치 수정

--- lib/view/screen/home/letter/letter.dart
+++ lib/view/screen/home/letter/letter.dart
@@ -35,6 +35,26 @@ class _LetterScreenState extends State<LetterScreen> {
                 ListTile(
                   iconColor: CustomColor.primary,
                   textColor: CustomColor.primary,
+                  leading: Icon(Icons.markunread_mailbox),
+                  title: Text(
+                    '한달 후 보낼 우편함',
+                    style: CustomTypography.bodyLarge.copyWith(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle:
+                      sendLettersCount == 0 ? Text('나에게 편지를 보내보세요.') : null,
+                ),
+                if (sendLettersCount > 0) ...{
+                  ListTile(
+                    title: Text('$sendLettersCount통이 준비되었습니다.'),
+                    subtitle: Text('조금만 기다려요. 곧 만날거에요!'),
+                  ),
+                },
+                Divider(),
+                ListTile(
+                  iconColor: CustomColor.primary,
+                  textColor: CustomColor.primary,
                   leading: Icon(Icons.inbox),
                   title: Text(
                     '나에게 받은 편지함',
@@ -105,25 +125,6 @@ class _LetterScreenState extends State<LetterScreen> {
                 ),
               ],
             ),
-            Divider(),
-            ListTile(
-              iconColor: CustomColor.primary,
-              textColor: CustomColor.primary,
-              leading: Icon(Icons.markunread_mailbox),
-              title: Text(
-                '한달 후 보낼 우편함',
-                style: CustomTypography.bodyLarge.copyWith(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              subtitle: sendLettersCount == 0 ? Text('나에게 편지를 보내보세요.') : null,
-            ),
-            if (sendLettersCount > 0) ...{
-              ListTile(
-                title: Text('$sendLettersCount통이 준비되었습니다.'),
-                subtitle: Text('조금만 기다려요. 곧 만날거에요!'),
-              ),
-            },
           ],
         ),
       ),
