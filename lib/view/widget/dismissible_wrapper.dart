8eaebedd07113d1f3fecd992e92ad089a0f72a68
Soo <soojlee0106@naver.com>
2024-11-06 02:36:03 +0900

    fix: 수정 스타일 추가

--- lib/view/widget/dismissible_wrapper.dart
+++ lib/view/widget/dismissible_wrapper.dart
@@ -6,10 +6,12 @@ import '../theme.dart';
 class DismissibleWrapperWidget extends StatelessWidget {
   final Key objectKey;
   final Function() onDismissed;
+  final Function()? onEdit;
   final Widget child;
 
   const DismissibleWrapperWidget({
     required this.objectKey,
+    this.onEdit,
     required this.onDismissed,
     required this.child,
     super.key,
@@ -21,37 +23,62 @@ class DismissibleWrapperWidget extends StatelessWidget {
 
     return Dismissible(
       key: objectKey,
+      direction: onEdit == null
+          ? DismissDirection.endToStart
+          : DismissDirection.horizontal,
       confirmDismiss: (DismissDirection direction) async {
-        return await showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text(tr.confirmDelete),
-              actions: <Widget>[
-                IconButton(
-                  color: CustomColor.primary,
-                  onPressed: () {
-                    Navigator.of(context).pop(true);
-                    onDismissed();
-                  },
-                  icon: const Icon(Icons.check),
-                ),
-              ],
-            );
-          },
-        );
+        if (direction == DismissDirection.endToStart) {
+          return await showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text(tr.confirmDelete),
+                actions: <Widget>[
+                  IconButton(
+                    color: CustomColor.primary,
+                    onPressed: () {
+                      Navigator.of(context).pop(true);
+                      onDismissed();
+                    },
+                    icon: const Icon(Icons.check),
+                  ),
+                ],
+              );
+            },
+          );
+        }
+
+        if (direction == DismissDirection.startToEnd) {
+          return await onEdit?.call();
+        }
+
+        return null;
       },
-      background: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(8.0),
-          child: Container(
-            color: CustomColor.warning.withOpacity(0.7),
-            child: const Icon(Icons.close, color: Colors.white),
-          ),
+      background: _buildBackground(DismissDirection.startToEnd),
+      secondaryBackground: _buildBackground(DismissDirection.endToStart),
+      child: child,
+    );
+  }
+
+  Widget _buildBackground(DismissDirection direction) {
+    final color = direction == DismissDirection.startToEnd
+        ? CustomColor.primary.withOpacity(0.7)
+        : CustomColor.warning.withOpacity(0.7);
+
+    final icon =
+        direction == DismissDirection.startToEnd ? Icons.edit : Icons.close;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: Container(
+          color: color,
+          alignment: Alignment.center,
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Icon(icon, color: Colors.white),
         ),
       ),
-      child: child,
     );
   }
 }
