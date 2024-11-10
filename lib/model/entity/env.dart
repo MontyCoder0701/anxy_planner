0ea1ee7c44ba8163944a1e48661b3d95c0135167
Soo <soojlee0106@naver.com>
2024-11-10 23:11:52 +0900

    fix: api 환경변수 추가

--- lib/model/entity/env.dart
+++ lib/model/entity/env.dart
@@ -5,8 +5,14 @@ part 'env.g.dart';
 @envied
 abstract class Env {
   @EnviedField(varName: 'ENCRYPT_KEY', obfuscate: true)
-  static String encryptKey = _Env.encryptKey;
+  static final String encryptKey = _Env.encryptKey;
 
   @EnviedField(varName: 'ENCRYPT_IV', obfuscate: true)
-  static String encryptIv = _Env.encryptIv;
+  static final String encryptIv = _Env.encryptIv;
+
+  @EnviedField(varName: 'ANDROID_API_KEY', obfuscate: true)
+  static final String androidApiKey = _Env.androidApiKey;
+
+  @EnviedField(varName: 'IOS_API_KEY', obfuscate: true)
+  static final String iosApiKey = _Env.iosApiKey;
 }
