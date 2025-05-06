30fbd5a661ed4cfdfe69ee6f18769903431f4912
Soo <soojlee0106@naver.com>
2025-05-06 17:34:23 +0900

    feat: 위젯 설정 추가

---
+++ ios/TodoWidgets/TodoWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TodoWidgetsBundle.swift
+//  TodoWidgets
+//
+//  Created by Soojeong Lee on 5/6/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TodoWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        TodoWidgets()
+    }
+}
